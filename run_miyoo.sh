ffbc91c543023cd26738835ce7cf9f4a27a21473
salvacam <salvacams@gmail.com>
2023-01-15 13:55:53 +0100

    make for Miyoo

---
+++ run_miyoo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export PATH=$PATH:/opt/miyoo/bin
+export TARGETMACH=arm-linux
+export BUILDMACH=i686-pc-linux-gnu
+export CROSS=arm-linux
+export CC=${CROSS}-gcc
+export LD=${CROSS}-ld
+export AS=${CROSS}-as
+export CXX=${CROSS}-g++
+make
